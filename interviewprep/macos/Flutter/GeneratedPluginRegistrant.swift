3995baf689f4b6fefdcbcab820322b9acee1d850
jo19lune <joachimloick939@gmail.com>
2026-06-13 11:42:00 +0300

    feat: mettre à jour les dépendances et ajouter le plugin flutter_tts

--- interviewprep/macos/Flutter/GeneratedPluginRegistrant.swift
+++ interviewprep/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_darwin
+import flutter_tts
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
